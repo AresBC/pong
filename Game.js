231cf98ee39693646d20779473c7a81f6f54c3c4
erik <erikhoanuer@web.de>
2021-01-27 20:43:40 +0100

    render Ball

--- Game.js
+++ Game.js
@@ -1,14 +1,44 @@
 let leftPlayerScore, rightPlayerScore;
 
+function px(num) {
+  return num + "px";
+}
+function pz(num) {
+  return num + "%";
+}
+function pt(num) {
+  return num + "pt";
+}
+
 function initalize() {
   gameField = new GameField(670, 450, "game");
   gameField.render();
+  ball = new Ball(50, gameField.getMiddle().width, gameField.getMiddle().height);
+  ball.render();
 }
 
+
+
 class Ball {
-  constructor(x, y) {
+  constructor(size, x, y) {
     this.x = x;
     this.y = y;
+    this.size = size;
+    this.setColor("blue");
+  }
+
+  setColor(color) {
+    this.color = color;
+  }
+
+  render() {
+    this.ball = document.createElement("div");
+    document.body.appendChild(this.ball);
+    this.ball.style.width = px(this.size);
+    this.ball.style.height = px(this.size);
+    this.ball.style.backgroundColor = this.color;
+    this.ball.style.borderRadius = pz(50);
+    this.ball.style.pos = pz(50);
   }
 }
 
@@ -27,6 +57,13 @@ class GameField {
     this.gameField.style.height = this.height + "px";
     this.gameField.style.backgroundColor = "red";
     this.gameField.style.margin = "20px";
+  }
+
+  getMiddle() {
+    return {
+      width: this.width / 2,
+      height: this.height / 2
+    };
   }
 }
 
